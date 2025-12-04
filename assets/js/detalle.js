aa80f1329bbdf5220da2482cb0bf4b455acd72e6
paulalopezv2 <paulalopezvaldes2@gmail.com>
2025-12-03 23:09:36 -0300

    detale.js version final

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -1,52 +1,51 @@
-// Obtener la ciudad desde la URL (versión simple)
+// obtener ciudad desde la url
 var parametros = new URLSearchParams(window.location.search);
 var ciudad = parametros.get("ciudad");
 
-// Decodificar por si trae espacios
-if (ciudad) {
+// por si viene con espacios
+if(ciudad){
     ciudad = decodeURIComponent(ciudad);
 }
 
-// Verificar si existe en los datos
-if (ciudad && climaData[ciudad]) {
+// revisar si existe
+if(ciudad && climaData[ciudad]){
 
     var data = climaData[ciudad];
 
-    // Título de la ciudad
+    // nombre ciudad
     document.querySelector(".ciudad-detalle").innerText = ciudad;
 
-    // Temperatura principal
+    // temperatura
     document.querySelector(".temp-detalle").innerText = data.temperatura + "°C";
 
-    // Icono principal
+    // icono
     document.querySelector(".icono-detalle").innerText = data.icono;
 
-    // Información adicional
+    // info de clima
     var info = "";
-    info += "<p>Humedad: <strong>" + data.humedad + "%</strong></p>";
-    info += "<p>Viento: <strong>" + data.viento + " km/h</strong></p>";
-    info += "<p>Sensación térmica: <strong>" + data.sensacion + "°C</strong></p>";
+    info += "<p>Humedad: <strong>"+data.humedad+"%</strong></p>";
+    info += "<p>Viento: <strong>"+data.viento+" km/h</strong></p>";
+    info += "<p>Sensación térmica: <strong>"+data.sensacion+"°C</strong></p>";
 
     document.querySelector(".info-extra").innerHTML = info;
 
-    // Pronóstico semanal
-    var contenedor = document.querySelector(".row");
-    contenedor.innerHTML = "";
+    // pronostico
+    var cont = document.querySelector(".row");
+    cont.innerHTML = "";
 
-    // Ciclo tradicional (más nivel M2)
-    for (var i = 0; i < data.pronostico.length; i++) {
+    for(var i=0; i < data.pronostico.length; i++){
         var d = data.pronostico[i];
 
-        contenedor.innerHTML +=
-            '<div class="col">' +
-                '<div class="mini-card">' +
-                    '<div class="dia">' + d.dia + '</div>' +
-                    '<div class="icono-mini">' + d.icono + '</div>' +
-                    '<div class="temp-mini">' + d.temp + '°C</div>' +
-                '</div>' +
-            '</div>';
+        cont.innerHTML +=
+        "<div class='col'>" +
+            "<div class='mini-card'>" +
+                "<div class='dia'>" + d.dia + "</div>" +
+                "<div class='icono-mini'>" + d.icono + "</div>" +
+                "<div class='temp-mini'>" + d.temp + "°C</div>" +
+            "</div>" +
+        "</div>";
     }
 
 } else {
-    console.log("Ciudad no encontrada");
+    console.log("no se encontro la ciudad");
 }
